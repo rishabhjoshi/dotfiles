3dcf822423b864135e9f85a0576c67f8711196ec
rishabhjoshi <rj04796@live.com>
2018-04-12 19:11:31 +0530

    corrected indicator

--- utils/indicate_memory_near_full.py
+++ utils/indicate_memory_near_full.py
@@ -9,5 +9,5 @@ import subprocess
 
 while True:
     if psutil.virtual_memory()[2] > 98:
-        subprocess.call("curl -h \"Content-type: application/json\" --data \"Done\" http://10.24.28.211:9999/jobComplete",
+        subprocess.call("curl -H \"Content-type: application/json\" --data \"Done\" http://10.24.28.211:9999/jobComplete",
                 shell = True)
