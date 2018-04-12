a1a2b9418a9bb612ba34274d3db617219c06a4aa
rishabhjoshi <rj04796@live.com>
2018-04-12 19:12:23 +0530

    updated time

--- utils/indicate_memory_near_full.py
+++ utils/indicate_memory_near_full.py
@@ -6,8 +6,10 @@
 
 import psutil
 import subprocess
+import time
 
 while True:
     if psutil.virtual_memory()[2] > 98:
         subprocess.call("curl -H \"Content-type: application/json\" --data \"Done\" http://10.24.28.211:9999/jobComplete",
                 shell = True)
+        time.sleep(3)
