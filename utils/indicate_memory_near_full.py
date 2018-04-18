7882c05dcabf342a4c88efe9ad36896b9eb5afbe
rishabhjoshi <rj04796@live.com>
2018-04-18 11:15:21 +0530

    integrated ram with slack

--- utils/indicate_memory_near_full.py
+++ utils/indicate_memory_near_full.py
@@ -8,15 +8,53 @@ import psutil
 import subprocess
 import time
 import argparse
+import requests
+import json
 
 parser = argparse.ArgumentParser(description='')
-parser.add_argument('-server', default='dosa')
+parser.add_argument('-server',      default= 'dosa',             help = 'Server running the script')
+parser.add_argument('-ram',         default= 98,     type = int, help = 'Ram Percentage Threshold')
+parser.add_argument('-ram_time',    default= 600,    type = int, help = 'Time to remind Ram in sec')
+
 args = parser.parse_args()
+channel = '#general'
+username = 'webhookbot'
+icon_emoji = ':ghost:'
+f = open('slack_url_file.txt')
+for line in f:
+    slackURL = line
+f.close()
+#slackURL = ''
+
+RAM_THRESH = args.ram
+RAM_REMINDER_TIME = args.ram_time
+DISK_THRESH = args.disk
+DISK_REMINDER_TIME = args.disk_time
+ONEG = 1024*1024*1024
 
 while True:
-    if psutil.virtual_memory()[2] > 97:
-        subprocess.call("curl -H \"Content-type: application/json\" --data \"{} in danger\" http://10.24.28.211:9999/jobComplete".format(args.server),
-                shell = True)
-        time.sleep(2)
+    if psutil.virtual_memory()[2] >= RAM_THRESH:
+        # subprocess.call("curl -H \"Content-type: application/json\" --data \"{} in danger\" http://10.24.28.211:9999/jobComplete".format(args.server),
+                # shell = True)
+        # time.sleep(2)
+        used = psutil.virtual_memory()[1] / ONEG
+        total = psutil.virtual_memory()[0] / ONEG
+        text = '{}\'s Memory nearly full. {0:.2f}GB of {0:.2f}GB used'.format(args.server, used, total)
+        data = {
+                'channel' : channel,
+                'username': username,
+                'text': text,
+                'icon_emoji' : icon_emoji
+        }
+        req = requests.post(slackURL, data = json.dumps(data), headers = {'Content-Type': 'application/json'})
+        if req.status_code != 200:
+                raise ValueError(
+                        'Request to slack returned an error %s, the response is:\n%s'
+                        % (req.status_code, req.text)
+                )
+                continue
+        else:
+                time.sleep(RAM_REMINDER_TIME)
+
     else: 
         time.sleep(60)
