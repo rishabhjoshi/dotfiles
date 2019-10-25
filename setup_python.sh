0b17970c2f923f85ece791046b2d0fc3febfd551
rishabhjoshi <rj04796@live.com>
2019-10-24 21:49:00 -0400

    correct execution of setup python'

--- setup_python.sh
+++ setup_python.sh
@@ -1,7 +1,8 @@
+#!/bin/bash
 pip3 install virtualenv
 cd $HOME/
 mkdir envs
 cd envs
 virtualenv myenv
-source myenv/bin/activate
+source ./myenv/bin/activate
 pip install numpy scipy matplotlib ipython jupyter pandas jupyterlab joblib scikit-learn pymongo gpustat nltk tables torch torchvision transformers seaborn
